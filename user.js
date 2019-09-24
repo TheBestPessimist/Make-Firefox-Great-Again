cc1d2f3535e642fdb46bab7faf5786952ab84263
TheBestPessimist <cristian@tbp.land>
2019-09-24 21:44:22 +0300

    Enable userChrome.css

--- user.js
+++ user.js
@@ -5,3 +5,4 @@ user_pref("browser.gesture.pinch.threshold", 450);
 user_pref("browser.tabs.closeTabByDblclick", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.screenshots.disabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
