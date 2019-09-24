8611fad5e2b5767f58718297082c05c3bb483137
TheBestPessimist <cristian@tbp.land>
2019-09-24 14:42:26 +0300

    Add user.js

---
+++ user.js
@@ -0,0 +1,7 @@
+ // https://support.mozilla.org/en-US/questions/1247072
+
+
+user_pref("browser.gesture.pinch.threshold", 450);
+user_pref("browser.tabs.closeTabByDblclick", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.screenshots.disabled", true);
