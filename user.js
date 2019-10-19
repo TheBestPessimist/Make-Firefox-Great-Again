62563d42ffa78adb301b484ff637ae053cf32950
TheBestPessimist <cristian@tbp.land>
2019-10-19 11:49:36 +0300

    Enable smooth zooming maybe

--- user.js
+++ user.js
@@ -4,3 +4,4 @@ user_pref("browser.tabs.closeTabByDblclick", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.screenshots.disabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("apz.allow_zooming", true);
