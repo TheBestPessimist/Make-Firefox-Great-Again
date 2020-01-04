5977a3d1c247789c984c70aaa0cf3578627c7347
TheBestPessimist <cristian@tbp.land>
2020-01-04 16:41:46 +0200

    Disable `apz.allow_zooming` as turning it on breaks zooming on so many sites :(

--- user.js
+++ user.js
@@ -4,4 +4,4 @@ user_pref("browser.tabs.closeTabByDblclick", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.screenshots.disabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("apz.allow_zooming", true);
+// user_pref("apz.allow_zooming", true); // turning this on breaks zooming on so many sites :(
