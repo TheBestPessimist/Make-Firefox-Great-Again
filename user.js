731443bc835e4213914336c3b4b9046e4c256cde
TheBestPessimist <cristian@tbp.land>
2020-04-08 09:22:45 +0300

    Disable the stupid urlbar onClick action starting with firefox 75.
    Why does that shit appear when i click the address bar? What if i did it by mistake? Now it have to move my cursor sooooo far away, out of that dropdown just to close it. WTF!??!
    
    That dropdown should only appear when i start typing something. Definitely not always when i click it.
    
    Turning `browser.urlbar.openViewOnFocus` to false fixes it for now.

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
-
 user_pref("browser.gesture.pinch.threshold", 450);
 user_pref("browser.tabs.closeTabByDblclick", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.screenshots.disabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_Pref("browser.urlbar.openViewOnFocus", false);
 // user_pref("apz.allow_zooming", true); // turning this on breaks zooming on so many sites :(
